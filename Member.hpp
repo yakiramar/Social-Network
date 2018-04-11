5b302dd0e437eb499eedee9ce4562ee29e23fa0b
yakiramar <33864769+yakiramar@users.noreply.github.com>
2018-04-12 00:25:18 +0300

    Update Member.hpp

--- Member.hpp
+++ Member.hpp
@@ -5,14 +5,14 @@ using namespace std;
 
 class Member{
     private:
-       static int counter;
+       static int counter;//count of members 
        vector<Member*> followers;//array of followers.
        vector<Member*> I_follow;//array of members that i follow.
-       int m_numFollowing;
-       int m_numFollowers;
+       int m_numFollowing;//count of members that i followe
+       int m_numFollowers;//count of number that followe after me
     public:
       
-       Member();
+       Member();//
        ~Member();
        void follow(Member &i_want_follow_after);
        void unfollow(Member &i_want_unfollow);
